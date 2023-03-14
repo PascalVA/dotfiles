d79eb1dc3d7e0b362fb27855934dd6597cd52ba4
Pascal Van Acker <vanacker.pascal@outlook.com>
2023-03-14 17:13:54 +0000

    install vundle and create .bashrc.d dir

--- setup.sh
+++ setup.sh
@@ -4,6 +4,26 @@
 THIS_REAL_FILE=$(readlink -e ${BASH_SOURCE[0]})
 THIS_REAL_DIR=$(dirname ${THIS_REAL_FILE})
 
+# add vundle plugin manager
+if [ ! -d "${HOME}/.vim/bundle/Vundle.vim" ]; then
+    git clone https://github.com/VundleVim/Vundle.vim.git ${HOME}/.vim/bundle/Vundle.vim
+fi
+
+# install kubectl aliasses
+if [ ! -d "${HOME}/github/ahmetb/kubectl-aliases" ]; then
+    git clone https://github.com/ahmetb/kubectl-aliases ${HOME}/github/ahmetb/kubectl-aliases
+fi
+
+# install alias completions
+if [ ! -d "${HOME}/github/cykerway/complete-alias" ]; then
+    git clone https://github.com/cykerway/complete-alias ${HOME}/github/cykerway/complete-alias
+fi
+
+# add bashrc custom dir
+if [ ! -d "${HOME}/.bashrc.d" ]; then
+    mkdir ${HOME}/.bashrc.d
+fi
+
 # symlink configuration files
 for _filename in .bashrc .profile .inputrc .tmux.conf .vimrc; do
     ln -sf ${THIS_REAL_DIR}/$_filename ${HOME}/$_filename
@@ -13,14 +33,10 @@ done
 if [ ! -d "${HOME}/.config/nvim" ]; then
      ${HOME}/.config/nvim
 fi
-ln -sf ${THIS_REAL_DIR}/alacritty.yml ${HOME}/.config/nvim/init.vim
+ln -sf ${THIS_REAL_DIR}/.vimrc ${HOME}/.config/nvim/init.vim
 
 # symlink alacritty config file
 if [ ! -d "${HOME}/.config/alacritty" ]; then
     mkdir ${HOME}/.config/alacritty
 fi
 ln -sf ${THIS_REAL_DIR}/alacritty.yml ${HOME}/.config/alacritty/alacritty.yml
-
-# clone handy repositories used in bashrc
-git clone https://github.com/ahmetb/kubectl-aliases ~/github/ahmetb/kubectl-aliases
-git clone https://github.com/cykerway/complete-alias ~/github/cykerway/complete-alias
