3e78a8c49dc56b539c9e9b36ba7b9668988f647f
Pascal Van Acker <vanacker.pascal@outlook.com>
2025-05-02 19:16:49 +0200

    Update setup script, alacritty and use ~/.local/bin instead of ~/bin

--- setup.sh
+++ setup.sh
@@ -1,23 +1,8 @@
 #!/usr/bin/env bash
 
-# real script path and parent directory name
-THIS_REAL_FILE=$(readlink -e ${BASH_SOURCE[0]})
-THIS_REAL_DIR=$(dirname ${THIS_REAL_FILE})
-
-# add local bashrc.d directory
-if [ ! -d "${HOME}/.bashrc.d" ]; then
-  mkdir ${HOME}/.bashrc.d
-fi
-
-# add local completions directory
-if [ ! -d "${HOME}/.bash_completion.d" ]; then
-  mkdir ${HOME}/.bash_completion.d
-fi
-
-# add vundle plugin manager
-if [ ! -d "${HOME}/.vim/bundle/Vundle.vim" ]; then
-  git clone https://github.com/VundleVim/Vundle.vim.git ${HOME}/.vim/bundle/Vundle.vim
-fi
+# store the parent directory path of this script so we
+# can use it for symlinking files later
+SCRIPT_DIR=$(dirname $(readlink -e ${BASH_SOURCE[0]}))
 
 # install kubectl aliasses
 if [ ! -d "${HOME}/github.com/ahmetb/kubectl-aliases" ]; then
@@ -29,39 +14,36 @@ if [ ! -d "${HOME}/github.com/cykerway/complete-alias" ]; then
   git clone https://github.com/cykerway/complete-alias ${HOME}/github.com/cykerway/complete-alias
 fi
 
-# install asdf
-if [ ! -d "${HOME}/.asdf" ]; then
-  git clone https://github.com/asdf-vm/asdf.git ${HOME}/.asdf --branch v0.11.2
-fi
+# install custom packages
+sudo apt install build-essential git libfuse2 pkg-config libfontconfig-dev direnv
+
+# install rust and cargo
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+
+# install cargo apps
+cargo install alacritty
+cargo install zellij
+
+# install azure-cli
+curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash
+
+# create custom directories in the user home directory
+mkdir -p ${HOME}/{.bashrc.d,.bash_completion.d}
+mkdir -p ${HOME}/.config/{alacritty,nvim}
+mkdir -p ${HOME}/.local/share/icons
 
 # symlink configuration files
-for _filename in .aliasses .bashrc .profile .inputrc .tmux.conf .vimrc; do
-  ln -sf ${THIS_REAL_DIR}/$_filename ${HOME}/$_filename
+for _filename in .aliasses .bashrc .inputrc .profile ; do
+  ln -sf "${SCRIPT_DIR}/$_filename" "${HOME}/$_filename"
 done
 
-# symlink the nvim config file
-if [ ! -d "${HOME}/.config/nvim" ]; then
-  mkdir -p ${HOME}/.config/nvim
-fi
-ln -sf ${THIS_REAL_DIR}/.vimrc ${HOME}/.config/nvim/init.vim
-
-# symlink alacritty config file
-if [ ! -d "${HOME}/.config/alacritty" ]; then
-  mkdir ${HOME}/.config/alacritty
-fi
-
-# add user bin dir
-if [ ! -d "${HOME}/bin" ]; then
-  mkdir ${HOME}/bin
-fi
-
 # copy and configure light-mode/dark-mode switcher
-ln -sf ${THIS_REAL_DIR}/bin/toggle-color-mode-gnome.sh ${HOME}/bin/toggle-color-mode-gnome.sh
+ln -sf ${SCRIPT_DIR}/.local/bin/toggle-color-mode-gnome.sh ${HOME}/.local/bin/toggle-color-mode-gnome.sh
 
 # configure alacritty
-ln -sf ${THIS_REAL_DIR}/.config/alacritty/alacritty.config.toml ${HOME}/.config/alacritty/alacritty.config.toml
-ln -sf ${THIS_REAL_DIR}/.config/alacritty/alacritty.dark.toml ${HOME}/.config/alacritty/alacritty.dark.toml
-ln -sf ${THIS_REAL_DIR}/.config/alacritty/alacritty.light.toml ${HOME}/.config/alacritty/alacritty.light.toml
+ln -sf ${SCRIPT_DIR}/.config/alacritty/alacritty.config.toml ${HOME}/.config/alacritty/alacritty.config.toml
+ln -sf ${SCRIPT_DIR}/.config/alacritty/alacritty.dark.toml ${HOME}/.config/alacritty/alacritty.dark.toml
+ln -sf ${SCRIPT_DIR}/.config/alacritty/alacritty.light.toml ${HOME}/.config/alacritty/alacritty.light.toml
 cat ${HOME}/.config/alacritty/alacritty.config.toml ${HOME}/.config/alacritty/alacritty.dark.toml >${HOME}/.config/alacritty/alacritty.toml
 
 #
