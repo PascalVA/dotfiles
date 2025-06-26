eebce08ea0191022894e03a0df93fa832073d9ec
Pascal Van Acker <vanacker.pascal@outlook.com>
2025-06-26 11:59:17 +0200

    Update color scheme toggling for new neovim config

--- .local/bin/toggle-color-mode-gnome.sh
+++ .local/bin/toggle-color-mode-gnome.sh
@@ -12,17 +12,21 @@ if [ "$color_scheme" = "'prefer-light'" ]; then
   gsettings set org.gnome.desktop.interface color-scheme prefer-dark
   cat ~/.config/alacritty/alacritty.config.toml ~/.config/alacritty/alacritty.dark.toml >~/.config/alacritty/alacritty.toml
   for nvim_server in $(ls /run/user/1000/nvim.*); do
-    nvim --server "$nvim_server" --remote-send ":colorscheme onehalfdark<CR>"
+    # nvim --server "$nvim_server" --remote-send ":colorscheme nightfox<CR>"
+    nvim --server "$nvim_server" --remote-send ":lua require('onedark').setup({ style = \"cool\", }) require('onedark').load()<CR>"
   done
-  sed -i 's/\(colorscheme =\) "[^"]\+"/\1 "onehalfdark"/' ~/.config/nvim/lua/plugins/colorscheme.lua
+  # sed -i 's/\(colorscheme =\) "[^"]\+"/\1 "nightfox"/' ~/.config/nvim/lua/plugins/colorscheme.lua
+  sed -i 's/\(style =\) "[^"]\+"/\1 "cool"/' ~/.config/nvim/lua/plugins/colorscheme.lua
   sed -i 's/\(theme\) .*/\1 "iceberg-dark"/' ~/.config/zellij/config.kdl
 else
   echo "toggle light"
   gsettings set org.gnome.desktop.interface color-scheme prefer-light
   cat ~/.config/alacritty/alacritty.config.toml ~/.config/alacritty/alacritty.light.toml >~/.config/alacritty/alacritty.toml
   for nvim_server in $(ls /run/user/1000/nvim.*); do
-    nvim --server "$nvim_server" --remote-send ":colorscheme onehalflight<CR>"
+    # nvim --server "$nvim_server" --remote-send ":colorscheme dayfox<CR>"
+    nvim --server "$nvim_server" --remote-send ":lua require('onedark').setup({ style = \"light\", }) require('onedark').load()<CR>"
   done
-  sed -i 's/\(colorscheme =\) "[^"]\+"/\1 "onehalflight"/' ~/.config/nvim/lua/plugins/colorscheme.lua
+  # sed -i 's/\(colorscheme =\) "[^"]\+"/\1 "dayfox"/' ~/.config/nvim/lua/plugins/colorscheme.lua
+  sed -i 's/\(style =\) "[^"]\+"/\1 "light"/' ~/.config/nvim/lua/plugins/colorscheme.lua
   sed -i 's/\(theme\) .*/\1 "catppuccin-latte"/' ~/.config/zellij/config.kdl
 fi
